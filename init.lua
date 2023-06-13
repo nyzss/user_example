5d825d01a57c0651ff0605fd321960f40a5bd310
nyzs <neazenmeh@gmail.com>
2023-06-14 00:27:25 +0300

    make powershell working

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
+
 return {
+ --
   -- Configure AstroNvim updates
   updater = {
     remote = "origin", -- remote to use
@@ -69,6 +71,18 @@ return {
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
   polish = function()
+    local powershell_options = {
+  shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
+  shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+  shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+  shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+  shellquote = "",
+  shellxquote = "",
+}
+
+for option, value in pairs(powershell_options) do
+  vim.opt[option] = value
+end
     -- Set up custom filetypes
     -- vim.filetype.add {
     --   extension = {
