c5bc4449c8f484eabaab52d3b7b0485b006b7114
nyzs <neazenmeh@gmail.com>
2023-06-14 05:19:13 +0300

    added flatten-nvim plug

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,16 +5,17 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- colorschemes:
-  { import = "astrocommunity.colorscheme.catppuccin",     lazy = false },
-  { import = "astrocommunity.colorscheme.nord-nvim",      lazy = false },
-  { import = "astrocommunity.colorscheme.oxocarbon-nvim", lazy = false },
-  { import = "astrocommunity.colorscheme.sonokai",        lazy = false },
-  { import = "astrocommunity.colorscheme.rose-pine",      lazy = false },
-  { import = "astrocommunity.colorscheme.kanagawa-nvim",  lazy = false },
-  { import = "astrocommunity.colorscheme.iceberg-vim",    lazy = false },
+  { import = "astrocommunity.colorscheme.catppuccin",           lazy = false },
+  { import = "astrocommunity.colorscheme.nord-nvim",            lazy = false },
+  { import = "astrocommunity.colorscheme.oxocarbon-nvim",       lazy = false },
+  { import = "astrocommunity.colorscheme.sonokai",              lazy = false },
+  { import = "astrocommunity.colorscheme.rose-pine",            lazy = false },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim",        lazy = false },
+  { import = "astrocommunity.colorscheme.iceberg-vim",          lazy = false },
   --
   -- other:
   { import = "astrocommunity.completion.codeium-vim" },
+  { import = "astrocommunity.terminal-integration.flatten-nvim" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   --
   --language packs:
