ee963db3834336c50d1fbfe292327467ece7fe7d
nyzs <neazenmeh@gmail.com>
2023-06-14 05:02:37 +0300

    added a couple of themes, plugins and settings.

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,12 +4,25 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.colorscheme.catppuccin" },
+  -- colorschemes:
+  { import = "astrocommunity.colorscheme.catppuccin", lazy = false },
+  { import = "astrocommunity.colorscheme.nord-nvim", lazy = false },
+  { import = "astrocommunity.colorscheme.oxocarbon-nvim", lazy = false },
+  { import = "astrocommunity.colorscheme.sonokai", lazy = false },
+  { import = "astrocommunity.colorscheme.rose-pine", lazy = false },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim", lazy = false },
+  { import = "astrocommunity.colorscheme.iceberg-vim", lazy = false },
+  --
+  -- other:
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  --
+  --language packs:
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.svelte" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.html-css" },
-
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.prisma" },
+  { import = "astrocommunity.pack.markdown" },
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -4,25 +4,39 @@ return {
   {
     "ray-x/lsp_signature.nvim",
     event = "BufRead",
-    config = function()
-      require("lsp_signature").setup()
-    end,
+    config = function() require("lsp_signature").setup() end,
+  },
+  {
+    "xiyaowong/transparent.nvim",
+    opts = {},
+    lazy = false,
+  },
+  {
+    "sainnhe/gruvbox-material",
+  },
+  {
+    "folke/twilight.nvim",
+    opts = {
+      dimming = {
+        alpha = 0.10,
+      },
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    -- lazy = false,
   },
   {
     "andweeb/presence.nvim",
     opts = {
       main_image = "file",
-      neovim_image_text = "go listen to kanye"
+      neovim_image_text = "go listen to kanye",
     },
-    lazy = false
+    lazy = false,
   },
   {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("todo-comments").setup {
-      }
-    end,
-
-  }
+    config = function() require("todo-comments").setup {} end,
+  },
 }
